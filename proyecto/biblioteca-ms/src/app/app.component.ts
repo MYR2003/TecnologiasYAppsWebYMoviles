3e0eb34879eeae1ab48be0e542a16372a35a9dbb
Antonio Méndez <149825617+amendezl@users.noreply.github.com>
2025-09-22 17:07:10 -0300

    A

--- proyecto/biblioteca-ms/src/app/app.component.ts
+++ proyecto/biblioteca-ms/src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet, IonHeader, IonToolbar, IonTitle, IonButtons, IonButton, IonIcon } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, IonHeader, IonToolbar, IonTitle, IonButtons, IonButton, IonIcon],
 })
 export class AppComponent {
-  constructor() {}
+  isDark = false;
+
+  toggleDarkMode() {
+    this.isDark = !this.isDark;
+    document.body.classList.toggle('dark', this.isDark);
+  }
 }
