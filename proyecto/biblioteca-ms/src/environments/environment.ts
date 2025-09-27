000b8331112fcce6c0a0bc488baf5f87f852a544
Antonio Méndez <149825617+amendezl@users.noreply.github.com>
2025-09-27 16:49:05 -0300

    Cambios

--- proyecto/biblioteca-ms/src/environments/environment.ts
+++ proyecto/biblioteca-ms/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   servicios: {
-    personas: 'http://localhost:3001',
+    personas: 'http://52.87.237.24:3001',
   medicos: 'http://localhost:3002',
   consultas: 'http://localhost:3004',
   fichasMedicas: 'http://localhost:3003'
