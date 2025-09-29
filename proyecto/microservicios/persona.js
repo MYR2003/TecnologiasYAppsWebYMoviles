25d06847fc13760efe56e9f5e7fa165e5fbf1d4c
Martin Esteban Yunge Rivas <martinyr24@gmail.com>
2025-09-29 16:10:29 -0300

    CRUD de persona
    CRUD de persona creado,

--- proyecto/microservicios/persona.js
+++ proyecto/microservicios/persona.js
@@ -17,6 +17,43 @@ app.get('/', async (req,res) => {
     }
 });
 
+app.post('/', async (req, res) => {
+    const {nombre, apellido, rut, fechaNacimiento, sistemaDeSalud, domicilio, telefono} = req.body
+    try {
+        const query = 'INSERT INTO persona(nombre, apellido, rut, fechaNacimiento, sistemaDeSalud, domicilio, telefono) VALUES ($1, $2, $3, $4, $5, $6, $7)';
+        const values = [nombre, apellido, rut, fechaNacimiento, sistemaDeSalud, domicilio, telefono]
+        const result = await SecurityPolicyViolationEvent.query(query, values);
+        res.json(result.rows)
+    } catch (error) {
+        throw error
+    }
+})
+
+// nombre, apellido, rut, fechaNacimiento, sistemaDeSalud, domicilio, telefono
+app.put('/', async (req, res) => {
+    // Por ahora se van a pedir todos los atributos para actualizar    
+    const {nombre, apellido, rut, fechaNacimiento, sistemaDeSalud, domicilio, telefono, idPersona} = req.body
+    try {
+        const query = 'UPDATE persona SET (nombre, apellido, rut, fechaNacimiento, sistemaDeSalud, domicilio, telefono) VALUES ($1, $2, $3, $4, $5, $6, $7) WHERE idPersona = $8 RETURNING *'
+        values = [nombre, apellido, rut, fechaNacimiento, sistemaDeSalud, domicilio, telefono, idPersona]
+        const result = await pool.query(query, values)
+        res.json(result.rows)
+    } catch (error) {
+        throw error
+    }
+})
+
+app.delete('/', async (req, res) => {
+    const {id} = req.body
+    try {
+        const query = 'DELETE FROM persona WHERE idPersona = $1'
+        await pool.query(query, [id])
+        res.json({message:'Exito en la eliminación de datos'})
+    } catch (error) {
+        throw error
+    }
+})
+
 app.listen(port, () => {
     console.log(`microservicio abierto en el puerto ${port}`)
 });
