fbcebb2fd74778474634fbc0e3820e03a3fb90fc
Antonio Méndez <149825617+amendezl@users.noreply.github.com>
2025-09-29 13:21:47 -0300

    Dashboards

--- proyecto/biblioteca-ms/src/app/app.routes.ts
+++ proyecto/biblioteca-ms/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./examenes/examenes.page').then(m => m.ExamenesPage),
+    loadChildren: () => import('./examenes/examenes.routes').then(m => m.routes),
     pathMatch: 'full',
   },
   {
---
+++ proyecto/biblioteca-ms/src/app/examenes/examenes.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { ExamenesPage } from './examenes.page';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: ExamenesPage,
+  },
+];
